8c6dd9e2edb3b7efce243744b11a2873e2fd7bfe
tony caine <tony.caine@water.nsw.gov.au>
2014-05-10 10:53:38 +1000

    1. load the txt file of power readings and trim to the 2 days required.
    2. convert the Date+Time fields to a dt field using Lubridate function

---
+++ load_power_data.R
@@ -0,0 +1,43 @@
+library(plyr)
+library(lubridate)
+
+dir<-'C:\\_store\\@stats\\data-course\\exploratory-analysis\\assess1-GIT\\ExData_Plotting1'
+setwd(dir)
+
+file<-'household_power_consumption.txt'
+
+power_all<-read.csv(file, header = TRUE, sep = ";", dec = ".", fill = TRUE, comment.char = "", na.strings= '?')
+
+str(power_all)
+head(power_all,3)
+
+
+head(power_all$Date)
+
+#We will only be using data from the dates 2007-02-01 and 2007-02-02.
+#but in the format that the dates were loaded as factors
+
+count(power_all$Date %in% c('1/2/2007', '2/2/2007'))
+
+power2Days<-power_all[ power_all$Date %in% c('1/2/2007', '2/2/2007') , ]
+head(power2Days$Date, 4)
+tail(power2Days$Date, 4)
+
+#clean out full dataframe
+rm(power_all)
+power2Days[,'dt'] <- dmy_hms(sprintf("%s %s", as.character(power2Days[, 'Date']), as.character(power2Days[,'Time'])))
+
+
+
+
+#d1<- power[1:10, 'Date']
+#d2<- as.character( d1)
+#t1<- power[1:10, 'Time']
+#t2<- as.character( t1)
+#sprintf("%s %s", d2,t2)
+
+#d3<-dmy(d2)
+#dt<-dmy_hms(sprintf("%s %s", d2,t2))
+
+power[,'dt'] <- dmy_hms(sprintf("%s %s", as.character(power[, 'Date']), as.character(power[,'Time'])))
+rm(power)
