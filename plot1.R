2afbb37d941a7ed4f945454e65947493387ecf21
tony caine <tony.caine@water.nsw.gov.au>
2014-05-10 15:35:38 +1000

    plotting directly to png. graph parameters are constants

--- plot1.R
+++ plot1.R
@@ -8,7 +8,18 @@ p.main='Global Active Power'
 p.xlab = 'Global Active Power (kilowatts)'
 p.ylab = 'Frequency'
 p.filename ='plot1.png'
+p.width=480
+p.height=480
+p.units="px"
+p.col='red'
   
-hist(power2days$Global_active_power, col="red", breaks = p.breaks, main=p.main, xlab=p.xlab, ylab=p.ylab)
-dev.copy(png, file=p.filename )
+#make it to the Window
+# hist(power2days$Global_active_power, col=p.col, breaks = p.breaks, main=p.main, xlab=p.xlab, ylab=p.ylab)
+# dev.copy(png, file=p.filename )
+# dev.off()
+
+#make it to the png directly - control the size
+png(filename = p.filename,
+    width = p.width, height = p.height, units = p.units)
+hist(power2days$Global_active_power, col=p.col, breaks = p.breaks, main=p.main, xlab=p.xlab, ylab=p.ylab)
 dev.off()
